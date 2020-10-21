87180558bcff4de15f81cef45a38d3f0804a0aa0
cmtradepro <admin@cmtrade.pro>
2020-10-21 12:22:17 +0800

    commit

--- src/pages/About.js
+++ src/pages/About.js
@@ -23,38 +23,6 @@ const Wrap = styled.div`
   text-align: center;
 `;
 
-const HandEmoji = styled.span`
-  animation-name: wave-animation;
-  animation-duration: 2.5s;
-  animation-iteration-count: infinite;
-  transform-origin: 70% 70%;
-  display: inline-block;
-
-  @keyframes wave-animation {
-    0% {
-      transform: rotate(0deg);
-    }
-    10% {
-      transform: rotate(-10deg);
-    }
-    20% {
-      transform: rotate(12deg);
-    }
-    30% {
-      transform: rotate(-10deg);
-    }
-    40% {
-      transform: rotate(9deg);
-    }
-    50% {
-      transform: rotate(0deg);
-    }
-    100% {
-      transform: rotate(0deg);
-    }
-  }
-`;
-
 const About = () => {
   return (
     <>
